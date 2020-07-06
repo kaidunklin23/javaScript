eecd841241d4602af95970fb38cfbfde8c8256a5
Kaija Dunklin <kaykay_s@yahoo.com>
2020-07-06 17:11:07 -0400

    first

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var donut =1
+var monkey =2
+var kaija =3
+var raja =4
+var audrey =5
+var shane =6
+var winston =7
+var apollo =8
+var florida =9
+var chcolate =10
+console.log(donut+florida)
+console.log(shane+audrey)
+console.log(chocolate-kaija)
+console.log(appolo-monkey)
+console.log(kaija*raja)
+console.log(florida*donut)
+console.log(chocolate/audrey)
+console.log(chocolate/monkey)
+console.log(raja+1)
+console.log(florida-2)
+
